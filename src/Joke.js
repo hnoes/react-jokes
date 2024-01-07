761c689d7bfec7b99404813e2e9e73def0f1379d
Hilarie <109528810+hnoes@users.noreply.github.com>
2024-01-06 21:18:34 -0500

    Add files via upload

---
+++ src/Joke.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import "./Joke.css";
+
+function Joke({ id, votes, text, vote }) {
+  const [voted, setVoted] = useState(false);
+
+  const handleVote = (delta) => {
+    if (!voted) {
+      vote(id, delta);
+      setVoted(true);
+    }
+  };
+
+  return (
+    <div className="Joke">
+      <div className="Joke-votearea">
+        <button onClick={() => handleVote(1)}>
+          <i className="fas fa-thumbs-up" />
+        </button>
+        <button onClick={() => handleVote(-1)}>
+          <i className="fas fa-thumbs-down" />
+        </button>
+        {votes}
+      </div>
+      <div className="Joke-text">{text}</div>
+    </div>
+  );
+}
+
+export default Joke;
